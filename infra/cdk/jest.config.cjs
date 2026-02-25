9e17e22afeeefbf828ce554a452cb5c86fa0a4be
jb <j@b.com>
2026-02-25 21:12:45 +0000

    TASK-022: implement IdentityStack

---
+++ infra/cdk/jest.config.cjs
@@ -0,0 +1,9 @@
+module.exports = {
+  testEnvironment: 'node',
+  roots: ['<rootDir>/test'],
+  testMatch: ['**/*.test.ts'],
+  transform: {
+    '^.+\\.ts$': ['ts-jest', { tsconfig: '<rootDir>/tsconfig.json' }],
+  },
+  moduleFileExtensions: ['ts', 'js', 'json'],
+};
